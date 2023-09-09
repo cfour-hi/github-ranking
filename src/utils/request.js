06075cf6f6121f4c38c2ca47c2683b55bd179ac4
cfour <cfour.zhou@gmail.com>
2023-09-09 15:40:40 +0800

    add GH_TOKEN

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ const request = axios.create({
 });
 
 request.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${token}`;
+  config.headers.Authorization = `Bearer ${process.env.GH_TOKEN}`;
   return config;
 });
 
